228cc0c260b021e52c268841b3db20a74d6cf194
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-07-10 14:53:14 -0400

    chore: fix eslint

--- src/routes/healthcheck.route.ts
+++ src/routes/healthcheck.route.ts
@@ -2,7 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/check",(req, res, next) =>
+router.get("/check",(req, res, next) => {
 res.status(200).json({ hello: 'world' })
-);
+});
+
 export default router;
